652ca2f3783ec214cf898507f2607f71c01b6510
Wonji Ko <wonjiko6718@gmail.com>
2019-12-05 18:38:23 +0900

    add main Camera
    .

---
+++ Assets/Scripts/mainCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainCam : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.position = target.transform.position - Vector3.forward;
+    }
+}
