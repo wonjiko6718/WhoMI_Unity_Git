b71ef412812a673bd8dce8d399cd7c1a841c661c
Wonji Ko <wonjiko6718@gmail.com>
2019-12-13 18:16:09 +0900

    add Title Scene
    fucntion for getting out application, and start the first Scene

---
+++ Assets/Scripts/ButtonScripts.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScripts : MonoBehaviour
+{
+    public void OnClickExit()
+    {
+        Application.Quit(); // if this button clicked, get exit application
+    }
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
