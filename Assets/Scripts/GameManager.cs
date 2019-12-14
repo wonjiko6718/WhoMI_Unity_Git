40f8605300eab483935fdec8e71191a67042a70b
Wonji Ko <wonjiko6718@gmail.com>
2019-12-14 17:38:04 +0900

    add GameOver Message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool playerDead = false;
+    
+    public static GameManager instance;
+    private GameObject playerIsNow;
+    public GameObject GameOverText;
 
+    private void Awake() 
+    {
+        if(GameManager.instance == null) // singleton
+        {
+            GameManager.instance = this;
+        }
+        playerDead = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        playerIsNow = GameObject.Find("Player");
+        if(playerIsNow == null)
+        {
+            playerDead = true;
+            GameOverText.SetActive(true);
+        }
+        else
+        {
+            playerDead = false;
+            GameOverText.SetActive(false);
+        }
+        OnGameOverMessage();
+    }
+    void OnGameOverMessage()
+    {
+        if(playerDead == true && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
     }
 }
