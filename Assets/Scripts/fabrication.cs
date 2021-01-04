6f23120caea44b0ac8051add3735000478e42a46
Wonji Ko <52885568+wonjiko6718@users.noreply.github.com>
2021-01-04 12:42:36 +0900

    add Rumor Jump Pattern

---
+++ Assets/Scripts/fabrication.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fabrication : MonoBehaviour
+{
+    private Transform fabricationTransform;
+    private Rigidbody2D fabricationRigid2D;
+    private int SpawnCount = 0;
+    private float fabricationHP = 100.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fabricationTransform = GetComponent<Transform>();
+        fabricationRigid2D = GetComponent<Rigidbody2D>();
+
+        fabricationTransform.localScale = new Vector3(2,2,2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRumor()
+    {
+        if(SpawnCount < 4)
+        {
+            // spawn Rumor
+            SpawnCount +=1;
+        }
+    }
+}
---
+++ Assets/Scripts/rumor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rumor : MonoBehaviour
+{
+    private Transform rumorTrans;
+    private Rigidbody2D rumorRigid2D;
+    private BoxCollider2D rumorCol2D;
+    private bool canJump = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rumorTrans = GetComponent<Transform>();
+        rumorRigid2D = GetComponent<Rigidbody2D>();
+        rumorCol2D = GetComponent<BoxCollider2D>();
+
+        rumorTrans.localScale = new Vector3(0.4f,0.4f,0.4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RumorMove();
+    }
+    void RumorMove()
+    {
+        if(canJump == true)
+        {
+            StartCoroutine(RumorJumpDelay());
+        }
+    }
+    IEnumerator RumorJumpDelay()
+    {
+        canJump = false;
+        rumorRigid2D.AddForce(new Vector2(0.0f,300.0f));
+        yield return new WaitForSeconds(1.5f); // RumorJumpDela
+        canJump = true;
+        
+    }
+}
