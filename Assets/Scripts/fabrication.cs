3505c544e451d183f6a9c830f76dfab5267b15ca
Wonji Ko <52885568+wonjiko6718@users.noreply.github.com>
2021-01-05 14:00:02 +0900

    add fabrication/rumor Moster
    fuction : move/spawn

--- Assets/Scripts/fabrication.cs
+++ Assets/Scripts/fabrication.cs
@@ -6,31 +6,53 @@ public class fabrication : MonoBehaviour
 {
     private Transform fabricationTransform;
     private Rigidbody2D fabricationRigid2D;
+    private BoxCollider2D fabricationBoxCollider2D;
+
     private int SpawnCount = 0;
+    private bool canSpawn = false;
     private float fabricationHP = 100.0f;
 
+    public GameObject fabricationSpawnedRumor;
 
     // Start is called before the first frame update
     void Start()
     {
         fabricationTransform = GetComponent<Transform>();
         fabricationRigid2D = GetComponent<Rigidbody2D>();
+        fabricationBoxCollider2D = GetComponent<BoxCollider2D>();
 
         fabricationTransform.localScale = new Vector3(2,2,2);
+        fabricationBoxCollider2D.size = new Vector2(4.0f,6.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void SpawnRumor()
-    {
-        if(SpawnCount < 4)
+        if(canSpawn == true && SpawnCount <= 2)
         {
-            // spawn Rumor
-            SpawnCount +=1;
+            StartCoroutine(SpawnRumorDelay());
         }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            canSpawn = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            canSpawn = false;
+        }
+    }
+    IEnumerator SpawnRumorDelay()
+    {
+        canSpawn = false;
+        GameObject spawningRumor = Instantiate(fabricationSpawnedRumor,transform.position,Quaternion.identity);
+        yield return new WaitForSeconds(1.5f);
+        SpawnCount ++;
+        canSpawn = true;
     }
 }
--- Assets/Scripts/rumor.cs
+++ Assets/Scripts/rumor.cs
@@ -7,8 +7,10 @@ public class rumor : MonoBehaviour
     private Transform rumorTrans;
     private Rigidbody2D rumorRigid2D;
     private BoxCollider2D rumorCol2D;
-    private bool canJump = true;
 
+    private float rumorMoveSpeed = 50.0f;
+    private bool canJump = true;
+    private bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
@@ -31,13 +33,33 @@ public class rumor : MonoBehaviour
         {
             StartCoroutine(RumorJumpDelay());
         }
+        if(canMove == true)
+        {
+            StartCoroutine(RumorMoveDelay());
+        }
     }
     IEnumerator RumorJumpDelay()
     {
         canJump = false;
         rumorRigid2D.AddForce(new Vector2(0.0f,300.0f));
-        yield return new WaitForSeconds(1.5f); // RumorJumpDela
+        yield return new WaitForSeconds(1.5f); // RumorJumpDelay
         canJump = true;
         
+    }
+    IEnumerator RumorMoveDelay()
+    {
+        canMove = false;
+        int rumorMoveDirection = Random.Range(-1,2); // -1,0,1 <- rumor move Direction
+        if(rumorMoveDirection == -1)
+        {
+            rumorTrans.localScale = new Vector3(0.4f,0.4f,0.4f);
+        }
+        else if (rumorMoveDirection == 1)
+        {
+            rumorTrans.localScale = new Vector3(-0.4f,0.4f,0.4f);
+        }
+        rumorRigid2D.AddForce(new Vector2(rumorMoveDirection * rumorMoveSpeed,0.0f)); // move
+        yield return new WaitForSeconds(1.5f);
+        canMove = true;
     }
 }
